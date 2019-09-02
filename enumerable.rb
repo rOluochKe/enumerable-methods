9a21d47d4998bbb472ba9919a0d3ef6c14dfadbc
Raymond Oluoch <rasholuoch@gmail.com>
2019-09-03 01:58:53 +0300

    enumerable methods done

---
+++ enumerable.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module Enumerable
+    #each
+    def my_each
+        i = 0
+        while i < self.length
+            yield(self[i])
+            i += 1
+        end
+    end
+
+    #each_with_index
+    def my_each_with_index
+        i = 0
+        x = 0
+        while i < self.length
+            yield(self[i], x)
+            i += 1
+        end
+    end
+
+    #select
+    def my_select 
+        result = []
+        self.my_each {|i| result.push(i) if yield(i)}
+        result
+    end
+    
+    #all?
+    def my_all?
+        self.my_each {|i| return false unless yield(i)}
+        true
+    end
+
+    #any?
+    def my_any?
+        result = false
+        self.my_each {|i| result = true if yield(i)}
+        result
+    end
+
+    #none
+    def my_none?
+        result = true
+        self.my_each {|i| result = false if yield(i)}
+        result
+    end
+
+    #count
+    def my_count
+        total = 0
+        self.my_each {|i| if (yield(i)) then total += 1 end}
+        return total
+    end
+
+    #map 
+    def my_map(&block)
+        result = []
+        self.my_each {|i| result.push(block.call(i))}
+        return result
+    end
+
+    #inject
+    def my_inject
+        m = self[0]
+        self.my_each {|i| m = yield(m, i)}
+        m
+    end
+
+    #multiply_els
+    def multiply_els(arr)
+        arr.my_inject {|x,y| x*y}
+    end
+end
+
+array = [2,4,5]
+puts multiply_els(array)
