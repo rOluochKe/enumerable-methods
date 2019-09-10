187ac10565218df51204cba7e7addbef31975fb1
Raymond Oluoch <rasholuoch@gmail.com>
2019-09-10 04:08:15 +0300

    enumerable changes - done

--- enumerable.rb
+++ enumerable.rb
@@ -1,78 +1,237 @@
-#!/usr/bin/ruby
+#==========METHODS=============
 
 module Enumerable
-    #each
+    
+#========my_each method======== 
+    
     def my_each
         i = 0
-        while i < self.length
-            yield(self[i])
-            i += 1
+        self.size.times do
+            yield(self[i])  
+            i += 1      
         end
-    end
-
-    #each_with_index
-    def my_each_with_index
-        i = 0
-        while i < self.count
-            yield(self[i],i)
-            i += 1
-        end
-
-    end
-
-    #select
-    def my_select 
-        result = []
-        self.my_each {|i| result.push(i) if yield(i)}
-        result
+    self
     end
     
-    #all?
+#========my_each_with_index method========   
+    
+    def my_each_with_index
+        i = 0
+          self.size.times do
+            yield(self[i], i)
+            i += 1
+        end 
+    self
+    end
+    
+#========my_select method========     
+    
+    def my_select
+        arr = []
+        self.my_each do |x|
+            if yield x
+                arr.push x
+            end
+        end
+        arr
+    end
+    
+#========my_all? method========     
+    
     def my_all?
-        self.my_each {|i| return false unless yield(i)}
-        true
+        arr = []
+        resul=true
+        self.my_each do |x|
+            if yield x
+                arr.push x
+            end
+        end
+        if arr.length == self.length
+            resul = true
+        else
+            resul = false
+        end
+        resul
     end
-
-    #any?
+    
+#========my_any? method========     
+    
     def my_any?
-        result = false
-        self.my_each {|i| result = true if yield(i)}
-        result
+        res = false
+        self.my_each do |x|
+            if yield x
+                res = true
+                break
+            end
+        end
+        res
     end
 
-    #none
+#========my_none? method========
+    
     def my_none?
-        result = true
-        self.my_each {|i| result = false if yield(i)}
-        result
+      res = true
+        self.my_each do |x|
+            if yield x
+                res = false
+                break
+            end
+        end
+        res  
+    end
+    
+#========my_count method========     
+    
+    def my_count value = nil
+        count = 0
+        self.my_each do|elem|
+            if value 
+                if elem == value 
+                    count += 1
+                end
+            elsif block_given? 
+                count += 1 if yield elem
+            else
+                count = self.length
+            end 
+        end
+        count
+    end
+    
+#========my_map method======== 
+    
+    def my_map
+        arr = []
+        self.my_each do |x| 
+            arr.push yield x
+        end
+        arr
     end
 
-    #count
-    def my_count
-        total = 0
-        self.my_each{total += 1}
-        total
+#========my_map_2 method(TAKES A PROC)========
+    
+    def my_map_2(my_proc)
+        arr = []
+        self.my_each do |x|
+          arr.push my_proc.call x
+        end
+        arr
+    end
+    
+#========my_map_3 method(TAKES EITHER A PROC OR A BLOCK)========     
+    
+    def my_map_3 my_proc = nil
+        arr = []
+        self.my_each do |x|
+            if my_proc == nil
+               arr.push yield x 
+            else
+                arr.push my_proc.call x
+            end
+        end
+        arr
     end
 
-    #map 
-    def my_map(&block)
-        result = []
-        self.my_each {|i| result.push(block.call(i))}
-        return result
+#========my_inject method======== 
+    
+    def my_inject index=0
+        count = index
+        self.my_each do|now|
+            count = yield count, now
+        end
+        count
     end
 
-    #inject
-    def my_inject
-        m = self[0]
-        self.my_each {|i| m = yield(m, i)}
-        m
-    end
 end
 
-#multiply_els
-def multiply_els(arr)
-    arr.my_inject {|x,y| x*y}
-end
 
-array = [2,4,5]
-puts multiply_els(array)
+#==================================TESTING INSTRUCTIONS============================
+#Each test is organized in order, uncomment the test block to run the test you need
+#==================================================================================
+
+
+#=======MY EACH METHOD TEST========
+#   array=[6,4,8,14,56,12,4,5,8]
+#   array.my_each do |num|
+#        num *= 2
+#        print "#{num} " 
+#   end
+#==================================
+
+
+#====== MY EACH WITH INDEX METHOD TEST ====================
+#    array=[6,4,8,14,56,12,4,5,8]
+#    array.my_each_with_index do |value, index|
+#        puts "Array index #{index} has the value #{value}"
+#    end
+#==========================================================
+
+
+#============== MY SELECT METHOD TEST =================
+#    array=[6,2,7,14,9,12,4,5,8]
+#    result = array.my_select do |n| n % 2 != 0 end
+#    p result
+#======================================================
+
+
+#============== MY ALL METHOD TEST ====================
+#    array=[6,2,4,14,6,12,4,3,8]
+#    res=array.my_all? do |n| n % 2 == 0 end
+#    print res
+#======================================================
+
+
+#============= MY ANY METHOD TEST =====================
+#    array=[6,4,8,14,56,12,1,4]
+#    res = array.my_any? do |n | n < 3 end
+#    print res
+#======================================================
+
+
+#============= MY NONE METHOD TEST ====================
+#    array=[6,4,8,14,56,12,4,1]
+#    res = array.my_none? do |n | n % 2 == 1 end
+#    print res
+#======================================================
+
+
+#============= MY COUNT METHOD TEST ===================
+#   array = [6,4,8,14,56,12,4,5,8,8,8,8,4,7,8,7,5]
+#   #res = array.my_count
+#   #res = array.my_count (8)
+#   res = array.my_count() do|n| n % 2 == 1  end
+#   p res
+#======================================================
+
+
+#============== MY MAP METHOD TEST ==================
+#    array=[6,4,8,14,56,12,4,5,8]
+#    res = array.my_map do |n| n * 2 end
+#    p res
+#====================================================
+
+
+#============== MY MAP 2 METHOD (TAKES A PROC) ==========
+#   array=[6,4,8,14,56,12,4,5,8]
+#   my_proc = Proc.new do |n| n * 2 end
+#   res = array.my_map_2(my_proc)
+#   p res
+#========================================================
+
+
+#============== MY MAP 3 METHOD (TAKES EITHER A PROC OR A BLOCK)=================
+#   array=[6,4,8,14,56,12,4,5,8]
+#   my_proc = Proc.new do |n| n * 2 end
+#   res = array.my_map_3 do |n| n * 3 end
+#   res = array.my_map_3(my_proc)
+#   p res
+#=================================================================================
+
+
+#=============== MY INJECT TEST =====================
+#   def multiply_els(array)
+#       array.my_inject(1) do |total, n|total * n end
+#   end
+#   array=[3,2,2,5]
+#   puts multiply_els(array)
+#====================================================
